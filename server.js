9bfc7a5f17f07455151dff9f8a87229f5d869a01
Yash Shah <yashshah@Yashs-MacBook-Air.local>
2025-09-07 05:04:37 -0700

    suii

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
